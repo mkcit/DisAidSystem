79cd2c5d5c6542c82947b6f53e3966c44f4937e0
mks.cit <mks.cit@outlook.com>
2025-11-03 23:37:52 +0200

    DistributionAidSystem

---
+++ app.js
@@ -0,0 +1,291 @@
+
+// port configuration
+const port = 3000;
+
+const fs = require('fs');
+
+const os = require('os');
+
+
+const path = require('path');
+
+// core
+const cors = require('cors');
+
+// database connection
+const db = require('./database').db;
+
+// database connection
+const insertData = require('./database').insertData;
+
+// database connection
+const fetchAll = require('./database').fetchAll;
+
+
+// database connection
+const createTable = require('./database').createTable;
+
+
+// database connection
+const updateReciving = require('./database').updateReciving;
+
+
+// database connection
+const fetchByHOF_ID = require('./database').fetchByHOF_ID;
+
+// database connection
+const deleteAllRows = require('./database').deleteAllRows;
+
+// database connection
+const dropTable = require('./database').dropTable;
+
+// uploading files settings
+const multer = require('multer');
+const uploadSettings = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 100 * 1024 * 1024 }
+});
+
+// excel files
+const XLSX = require('xlsx');
+
+// express app setup
+const express = require('express');
+const app = express();
+
+// setting up the view engine
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
+
+app.get('/uploadData', (req, res, next) => {
+    res.render('uploadData');
+});
+
+app.post('/uploadData', uploadSettings.single('excelFile'), (req, res, next) => {
+
+
+    if (!req.file) {
+        // 400 File not Found
+        return res.status(400).render('fileError', {
+            title: 'File Not Found',
+            admin: true
+        });
+    }
+
+    if (req.file.size >= 10 * 1024 * 1024) {
+        // 400 File not Found
+        return res.status(400).render('fileError', {
+            title: 'File is too big!, It must be less than 10MB',
+            admin: true
+        });
+    }
+
+    const mimeType1 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'; // xlsx
+    const mimeType2 = 'application/vnd.ms-excel'; // xls
+    if (req.file.mimetype !== mimeType1 && req.file.mimetype !== mimeType2) {
+        // 415 File not supported
+        return res.status(415).render('fileError', {
+            title: 'File Not Supported',
+            admin: true
+        });
+    }
+
+    // اقرأ الملف من الـ buffer
+    const workbook = XLSX.read(req.file.buffer, { type: 'buffer' });
+
+    // خُذ أول ورقة
+    const sheetName = workbook.SheetNames[0];
+    if (!sheetName) {
+        return res.status(400).render('fileError', {
+            title: 'File does not have a sheet!',
+            admin: true
+        });
+    }
+
+    const sheet = workbook.Sheets[sheetName];
+    const rows = XLSX.utils.sheet_to_json(sheet, { defval: '', raw: false });
+
+    const result = insertData(rows);
+    if (result.type == 1) {
+        return res.redirect('/allData');
+
+    } else {
+        return res.status(400).render('fileError', {
+            title: result.message,
+            admin: true
+        });
+    }
+
+});
+
+app.get('/export', (req, res, next) => {
+    exportExcelFile(res, false);
+});
+
+const exportExcelFile = (res, isDropping) => {
+    const result = fetchAll();
+    if (result.type == 1) {
+        const wb = XLSX.utils.book_new();
+        const ws = XLSX.utils.json_to_sheet(result.data, { cellDates: true });
+
+        XLSX.utils.book_append_sheet(wb, ws, 'Sheet');
+
+        const PATH = path.join(os.homedir(), 'Downloads');
+        const FILE_NAME = PATH + '/' + getDateTime();
+        const buf = XLSX.write(wb, { bookType: 'xlsx', type: 'buffer' });
+        fs.writeFile(FILE_NAME, buf, (err) => {
+            if (err) {
+                return res.status(400).render('fileError', {
+                    title: err.message || 'Error in exporting file!',
+                    admin: true
+                });
+            }
+
+            if (!isDropping)
+                res.status(200).redirect('/');
+            else {
+                const result1 = dropTable();
+                if (result1.type == 1) {
+                    res.redirect('/');
+                } else {
+                    res.status(400).render('fileError', {
+                        title: result1.message || 'Error in data processing!',
+                        admin: true
+                    });
+                }
+            }
+
+        });
+    }
+    else {
+        res.status(400).render('fileError', {
+            title: result.message || 'Error in data processing!',
+            admin: true
+        });
+    }
+}
+
+const getDateTime = function () {
+    return `benef_${new Date().toLocaleString().replace(/[/:\\,]/g, '-')}.xlsx`;
+
+}
+
+app.get('/allData', (req, res, next) => {
+    const result = fetchAll();
+
+    if (result.type == 1) {
+        res.status(200).render('allData', {
+            rows: result.data,
+            title: 'All Beneficiaries'
+        });
+    } else {
+        res.status(400).render('fileError', {
+            title: result.message || 'Error in processing Data!',
+            admin: true
+        });
+    }
+});
+
+app.post('/drop', (req, res, next) => {
+    exportExcelFile(res, true);
+});
+
+app.post('/createTable', (req, res, next) => {
+    const result = createTable();
+    if (result.type == 1) {
+        res.redirect('/');
+    } else {
+        res.status(400).render('fileError', {
+            title: result.message || 'Error in data processing!',
+            admin: true
+        });
+    }
+});
+
+app.post('/deleteAll', (req, res, next) => {
+    const result = deleteAllRows();
+    if (result.type == 1) {
+        res.redirect('/');
+    } else {
+        res.status(400).render('fileError', {
+            title: result.message || 'Error in data processing!',
+            admin: true
+        });
+    }
+});
+
+
+app.get('/api/search', (req, res) => {
+    res.status(200).render('search', {
+        title: 'Distribuation Aid System',
+        result: null,
+        rowsCount: 0,
+        error: null,
+        afterConfirmation: false,
+    });
+});
+
+app.post('/api/search', (req, res) => {
+    const hof_id = req.body.hof_id;
+    const result = fetchByHOF_ID(hof_id);
+
+    if (result.type == 1) {
+        const rowsCount = result.data.length;
+        res.status(200).render('search', {
+            title: 'Distribuation Aid System',
+            result: result.data,
+            rowsCount: rowsCount,
+            error: null,
+            afterConfirmation: false,
+        })
+    } else {
+        res.status(400).render('fileError', {
+            title: result.message || 'Error in data processing!',
+            admin: false
+        });
+    }
+
+});
+
+app.post('/api/confirm', (req, res) => {
+    const hof_id = req.body.hof_id;
+    const receiver_name = req.body.receiver_name;
+    const hof_fullname = req.body.HOF_FullName;
+
+    const hostname = os.hostname();
+
+    const result = updateReciving(hof_id, receiver_name, hostname);
+    if (result.type == 1) {
+
+        res.status(200).render('search', {
+            title: 'Distribuation Aid System',
+            result: null,
+            rowsCount: 0,
+            error: null,
+            afterConfirmation: true,
+            hof_fullname: hof_fullname
+
+        })
+    } else {
+        res.status(400).render('fileError', {
+            title: result.message || 'Error in data processing!',
+            admin: false
+        });
+    }
+
+});
+
+app.use('/', (req, res, next) => {
+    res.status(200).render('index', {
+        title: 'Main Page'
+    });
+});
+
+
+app.listen(port, '0.0.0.0', () => {
+    console.log('0.0.0.0 is active..')
+});
---
+++ database.js
@@ -0,0 +1,219 @@
+const DataBase = require('better-sqlite3');
+const db = new DataBase('disaid.db');
+db.pragma('journal_mode = WAL');   // Enable Write-Ahead Logging
+db.pragma('synchronous = NORMAL'); // Set synchronous mode to NORMAL for better performance
+
+const TABLE_NAME = 'beneficiary'
+
+
+
+const createTable = function () {
+  try {
+
+    db.exec(`
+      CREATE TABLE IF NOT EXISTS ${TABLE_NAME} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+        HOF_ID TEXT NOT NULL,
+        HOF_FullName TEXT NOT NULL,
+        Mobile TEXT,
+        Center_Name TEXT,
+
+        Member_FullName TEXT,
+        Member_ID TEXT,
+        Quantity INTEGER DEFAULT 1,
+        
+        IsReceived INTEGER DEFAULT 0 ,
+        DeliveryTime DATETIME NULL,
+        RecieverName TEXT NULL,
+        DeviceName TEXT NULL,
+
+        date TEXT DEFAULT (datetime('now')),
+
+
+        
+        Member_DateBirth TEXT
+      );
+
+      CREATE INDEX IF NOT EXISTS idx_idp_hofid ON ${TABLE_NAME} (HOF_ID);
+      CREATE INDEX IF NOT EXISTS idx_idp_memberid ON ${TABLE_NAME} (Member_ID);
+    `);
+
+    return {
+      type: 1,
+      message: "success"
+    };
+  }
+  catch (err) {
+    return {
+      type: -1,
+      message: err.message,
+      code: err.code || null,
+    };
+  }
+}
+
+
+
+const insertData = function (rows) {
+
+  try {
+    const insert = db.prepare(`INSERT INTO ` + TABLE_NAME + ` (
+                        HOF_ID, HOF_FullName, Member_FullName, Member_ID,
+                        Member_DateBirth, Center_Name, Mobile, date
+                    ) VALUES (?, ?, ?, ?, ?, ?, ?, datetime('now'))`);
+
+    const deleteAll = db.prepare(`DELETE FROM ` + TABLE_NAME);
+
+    const transaction = db.transaction((rows) => {
+      deleteAll.run();
+      rows.forEach(row => {
+        const hof_id = row['HOF ID'];
+        const hof_fullname = row['HOF FullName'];
+        const member_fullName = row['Member FullName'];
+        const member_id = row['Member ID'];
+        const memeber_datebirth = row['Member Date Birth'];
+        const center_name = row['Center Name'];
+        const mobile = row['Jawwal#'];
+
+        insert.run(hof_id,
+          hof_fullname,
+          member_fullName,
+          member_id,
+          memeber_datebirth,
+          center_name,
+          mobile);
+      });
+    });
+
+    transaction(rows);
+    const result = {
+      type: 1,
+      message: "success"
+    };
+    return result;
+  }
+  catch (err) {
+    const result = {
+      type: -1,
+      message: err.message,
+      code: err.code || null,
+    };
+    return result;
+  }
+}
+
+const dropTable = function () {
+  try {
+    db.exec('DROP TABLE ' + TABLE_NAME + ';');
+    const result = {
+      type: 1,
+      message: "success"
+    };
+    return result;
+  } catch (err) {
+    const result = {
+      type: -1,
+      message: err.message,
+      code: err.code || null,
+    };
+    return result;
+  }
+}
+
+const deleteAllRows = function () {
+  try {
+    db.exec('DELETE FROM ' + TABLE_NAME + ';');
+    const result = {
+      type: 1,
+      message: "success"
+    };
+    return result;
+  } catch (err) {
+    const result = {
+      type: -1,
+      message: err.message,
+      code: err.code || null,
+    };
+    return result;
+  }
+}
+
+const fetchAll = function () {
+
+
+  // const select = db.prepare('SELECT HOF_ID,HOF_FullName,Member_FullName,Member_ID,Center_Name,Mobile FROM ' + TABLE_NAME + ';');
+
+  try {
+    const select = db.prepare('SELECT * FROM ' + TABLE_NAME + ';');
+    const data = select.all();
+    const result = {
+      type: 1,
+      message: "success",
+      data: data
+    };
+    return result;
+  } catch (err) {
+    const result = {
+      type: -1,
+      message: err.message,
+      code: err.code || null,
+      data: null
+    };
+    return result;
+  }
+}
+
+
+const fetchByHOF_ID = function (hof_id) {
+
+  // const select = db.prepare('SELECT HOF_ID,HOF_FullName,Member_FullName,Member_ID,Center_Name,Mobile FROM ' + TABLE_NAME + ';');
+
+  try {
+    const select = db.prepare('SELECT * FROM ' + TABLE_NAME + ' WHERE HOF_ID = ? ;');
+    const data = select.all(String(hof_id).trim());
+    const result = {
+      type: 1,
+      message: "success",
+      data: data
+    };
+    return result;
+  } catch (err) {
+    const result = {
+      type: -1,
+      message: err.message,
+      code: err.code || null,
+      data: null
+    };
+    return result;
+  }
+}
+
+
+const updateReciving = function (hof_id, receiver_name, hostname) {
+
+  try {
+
+    const update = db.prepare(`UPDATE  ` + TABLE_NAME + ` SET 
+    IsReceived=1 , DeliveryTime=datetime('now', 'localtime'), RecieverName=? ,DeviceName = ? WHERE HOF_ID=?`);
+
+    const info = update.run(receiver_name, hostname, hof_id);
+    const result = {
+      type: 1,
+      message: "success",
+      info: info
+    };
+    return result;
+  }
+  catch (err) {
+    const result = {
+      type: -1,
+      message: err.message,
+      code: err.code || null,
+      info: info
+    };
+    return result;
+  }
+}
+
+module.exports = { db, insertData, dropTable, deleteAllRows, fetchAll, fetchByHOF_ID, updateReciving, createTable };
